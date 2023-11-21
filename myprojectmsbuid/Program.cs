d33a61354470e9eb13e5c3d331ae873763c244ac
sujeet0325 <142496120+sujeet0325@users.noreply.github.com>
2023-11-21 16:05:38 +0530

    Initial commit

---
+++ myprojectmsbuid/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myprojectmsbuid
+{
+    internal class Program
+    {
+        public void sushant()
+        {
+            Console.WriteLine(  "welcome to ms buid");
+        }
+        static void Main(string[] args)
+        {
+            Program p= new Program();   
+            p.sushant();
+            Console.ReadLine();
+        }
+    }
+}
